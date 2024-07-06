a3930712cdcffb0df313ff77cfc734d0a1c5350d
NayeemAhsan <mnahsan21@gmail.com>
2024-07-05 20:36:17 -0400

    updated post_request.py file to retrieve status code and updated live_post screen shot

--- src/api/post_request.py
+++ src/api/post_request.py
@@ -19,5 +19,11 @@ post_url = url+"/predict"
 
 data = config["sample_data"]["data_above_50k"]
 
+logger.info("Sending POST request")
 response = requests.post(post_url, json=data)
+
+# Print the response status code
+print(f"Status Code: {response.status_code}")
+
+# Print the response JSON
 print(response.json())
